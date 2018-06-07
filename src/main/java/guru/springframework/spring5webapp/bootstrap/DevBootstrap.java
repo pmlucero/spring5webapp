02c8ab56dff42bb90e67a4fd7f03ec4fcf06d684
Pablo Lucero <pablo.lucero@globallogic.com>
2018-06-07 09:31:25 -0500

    Spring Data JPA - inclusión de Publisher.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,30 +15,44 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     private void initData() {
 
         //Pablo
         Author pablo = new Author("Pablo", "Lucero");
-        Book b1 = new Book("mybook", "1234", "editorial b1");
+
+        Publisher p1 = new Publisher("Pedro", "Picapiedra",
+                "Av.Corrientes", "485", "2",
+                null, "Buenos Aires", "Argentina", "C1043AAE");
+
+        Book b1 = new Book("mybook", "1234", p1);
         pablo.getBooks().add(b1);
         b1.getAuthors().add(pablo);
 
         authorRepository.save(pablo);
+        publisherRepository.save(p1);
         bookRepository.save(b1);
 
         //Fran
         Author fran = new Author("Fran", "Lucero");
-        Book b2 = new Book("secondbook", "2233", "editorial aaa");
+
+        Publisher p2 = new Publisher("Pablo", "Marmol",
+                "Av.Corrientes", "485", "2",
+                null, "Buenos Aires", "Argentina", "C1043AAE");
+
+        Book b2 = new Book("secondbook", "2233", p2);
         fran.getBooks().add(b2);
         b2.getAuthors().add(fran);
 
         authorRepository.save(fran);
+        publisherRepository.save(p2);
         bookRepository.save(b2);
     }
 
---
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -0,0 +1,149 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String addressNumber;
+    private String floor;
+    private String apartment;
+    private String city;
+    private String country;
+    private String zipcode;
+
+    public Publisher() {}
+
+    public Publisher(String firstName, String lastName, String address, String addressNumber, String floor, String apartment, String city, String country, String zipcode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.addressNumber = addressNumber;
+        this.floor = floor;
+        this.apartment = apartment;
+        this.city = city;
+        this.country = country;
+        this.zipcode = zipcode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddressNumber() {
+        return addressNumber;
+    }
+
+    public void setAddressNumber(String addressNumber) {
+        this.addressNumber = addressNumber;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return id != null ? id.equals(publisher.id) : publisher.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", addressNumber='" + addressNumber + '\'' +
+                ", floor='" + floor + '\'' +
+                ", apartment='" + apartment + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                '}';
+    }
+}
